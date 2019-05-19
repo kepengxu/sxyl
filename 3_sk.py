650da3b5f3613d281949f36e52a79e673bea64c7
cooper <xkp793003821@outlook.com>
2019-05-19 23:25:39 +0800

    xkp793003821@outlook.com

--- 3_sk.py
+++ 3_sk.py
@@ -4,23 +4,47 @@ from sklearn.model_selection import train_test_split
 from dataprocess import get_data_two,get_data_three
 from sklearn.metrics import precision_recall_curve,precision_score,recall_score,f1_score
 import numpy as np
-x,y=get_data_three('/home/cooper/PycharmProjects/sxyl/Assignments/iris3.txt')
+import keras
+from dataprocess import get_data_one
+from keras.models import Sequential,Model,Input
+from keras.callbacks import TensorBoard,ModelCheckpoint
+from keras.layers import Dense
+from keras.metrics import *
+from keras.losses import mse,categorical_crossentropy
+
+X,Y=get_data_three('/home/cooper/PycharmProjects/sxyl/Assignments/iris3.txt')
 stand=StandardScaler()
-x=stand.fit_transform(x)
-onehot=OneHotEncoder()
-y=onehot.fit_transform(y)
-print(x.shape,y.shape)
-
-x_train,x_test,y_train,y_test=train_test_split(x,y,test_size=0.2)
+X=stand.fit_transform(X)
+# onehot=OneHotEncoder()
+# Y=onehot.fit_transform(Y)
+print(X.shape,Y.shape)
+# Y=np.asarray(Y,np.int)
+YY=np.zeros([150,3],np.int64)
+for i in range(Y.shape[0]):
+    YY[i,Y[i]]=1
+print(X.shape,YY.shape)
+x_train,x_test,y_train,y_test=train_test_split(X,YY,test_size=0.2,random_state=1)
 print(x_train.shape,x_test.shape,y_train.shape,y_test.shape)
-model = LogisticRegressionCV(fit_intercept=True, Cs=np.logspace(-5, 1, 100),
-                          multi_class='multinomial', penalty='l2', solver='lbfgs')
 
-model.fit(x_train,y_train)
 
-y_pre=model.predict(x_test)
-print('recall: ',recall_score(y_test,y_pre))
-print('precision',precision_score(y_test,y_pre))
-print('f1_score',f1_score(y_test,y_pre))
 
+def getmodel(inunit,outunit):
+    x=Input(shape=(4,))
+    o=Dense(outunit,activation='softmax')(x)
+    model=Model(x,o,name='model')
+    return model
+
+model=getmodel(4,3)
+
+ten=TensorBoard()
+
+model.compile(optimizer='sgd',loss=categorical_crossentropy,metrics=[categorical_accuracy])
+
+# x,y=get_data_one('/home/cooper/PycharmProjects/sxyl/Assignments/diabetes.txt')
+model.fit(x_train,y_train,
+          batch_size=32,
+          epochs=10000,
+          callbacks=[ten],validation_data=[x_test,y_test])
+model.save_weights('3_weight_model.hdf5')
+# model.evaluate(x_test,y_test)
 
